f29ff1dcd0ef1ea2c6127cddc80e9b7365ace9da
WoodsDos <javier.gonzales.2202@gmail.com>
2026-05-13 22:04:50 -0500

    feat: configure angular frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
+import { HomeComponent } from './public/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+
+  { path: '**', component: PageNotFoundComponent },
+];
---
+++ src/app/public/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+// home.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { Router } from '@angular/router'; // Importar Router
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  name = 'home';
+
+  // Inyecta Router en el constructor
+  constructor(private router: Router) {}
+
+
+}
